fc47726737a94754a2f3d40de28380da1d2d1d47
malathi <malathic0798@gmail.com>
2024-11-14 10:03:09 +0530

    PSD-18: only numeric data typesare covered

---
+++ basics/prac_datatypes.py
@@ -0,0 +1,35 @@
+#Numeric - int, float, complex
+a = 1
+print(a)
+print(type(a))
+print(id(a))
+b = 500
+print(b)
+print(type(b))
+print(id(b))
+
+c = 2.2
+print("c valueis : ",c)
+print("c typeis : ", type(c))
+print("c add is : ", id(c), "hello", type(a))
+d = 0.1
+print(d)
+print(type(d))
+print(id(d))
+
+e = 2 + 3j
+print(e)
+print(type(e))
+print(id(e))
+print(e.imag)
+print(e.real)
+
+
+f = 26 + 33j
+print(f)
+print(type(f))
+print(id(f))
+print(f.imag)
+print(f.real)
+
+
