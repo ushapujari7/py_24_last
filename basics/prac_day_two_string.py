45db3443c890e727ae18d931a036adff41875b5f
malathi <malathic0798@gmail.com>
2024-11-18 10:02:42 +0530

    string explained

--- basics/prac_day_two_string.py
+++ basics/prac_day_two_string.py
@@ -89,6 +89,104 @@ enst = "My name is Ståle"
 en1 = enst.encode()
 print(en1)
 
+st = "Hi hello how are you nithin, class are taken by Nithin"
+st1 = st.count("i")
+print(st1)
+print(st.count("I"))
+print(st.count("Ni"))
+print(st.count("in"))
+
+
+# format_map, join ??????????????????????????????????
+
+nst = 'its nithin\'s class'
+nst2 = '''its nithin's class'''
+nst3 = "its nithin's class"
+nst4 = 'its nithin"s class'
+
+# its nithin's class
+
+print(*[nst, nst2, nst3, nst4], sep="\n", end='*********\n')
+
+
+
+
+si1 = "my name is Nagendra, im here to learn SQL"
+si2 = "my name is Venkat, im here to learn python"
+si3 = "my name is usha, im here to learn python & django"
+
+print(*(si1, si2, si3), sep="\n")
+
+sid = "my name is {0}, im here to learn {1}"
+si_na = sid.format("Nagendra", "SQL")
+print(si_na)
+si_ve = sid.format("Venkat", "python")
+print(si_ve)
+
+
+
+name = "Saikiran"
+cou = "Full stack"
+fst = f"my name is {name}, im here to learn {cou}"
+print(fst)
+
+
+
+s_st = "Hi my name is Nithin, here Im teaching Pyhton"
+print(s_st[0])
+print(s_st[20])
+print(s_st[3:10]) #print(s_st[inc:exe])
+print(s_st[0:26])
+print(s_st[:26])
+print(s_st[3:])
+print(s_st[3:500])
+print(s_st[-6:])
+print(s_st[-6:200])
+
+
+im_s = "Nithin"
+he = "Nithin"
+hs = "nithin"
+print(id(im_s))
+print(id(he))
+print(id(hs))
+
+hh = he.upper()
+print(he.upper())
+print(hh)
+print(he)
+
+
+li = [1, 2, 3]
+li2 = [1, 2, 3]
+li.append(10)
+print(id(li))
+print(id(li2))
+print(li)
+
+
+
+s = "lkfdlk"
+print(dir(s))
+print(help(str))
+i = 2
+print(dir(i))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
